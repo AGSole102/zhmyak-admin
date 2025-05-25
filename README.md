ce4613b0e5e114293662e486ccec663fc47318e6
Aleksandr Fadeev <333702@niuitmo.ru>
2025-05-25 21:19:47 +0300

    new admin repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# Zhmyak Admin-Panel
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+  ],
+}
